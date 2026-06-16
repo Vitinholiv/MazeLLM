c8f2fda2cd297e9521f30e5f7a9e02afd5fe16bf
VitinDenoyr <vitordenoyr@gmail.com>
2026-06-16 19:57:51 -0300

    feat: Preprocessing adjusted to dencoder model

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,88 +1,72 @@
-import os
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
 class MazeTokenizer:
     def __init__(self):
-        self.char_to_id = {
-            '<PAD>': 0,   # padding
-            '#':     1,   # wall
-            ' ':     2,   # open path
-            'S':     3,   # start cell
-            'E':     4,   # end cell
-            '\n':    5,   # row separator inside maze grid
-            '&':     6,   # maze separator
-            'L':     7,   # direction: Left
-            'R':     8,   # direction: Right
-            'U':     9,   # direction: Up
-            'D':    10,   # direction: Down
-        }
+        self.special = {'<PAD>': 0, '<START>': 1, '<END>': 2, '<SEP>': 3}
+        self.grid    = {'#': 4, ' ': 5, 'S': 6, 'E': 7, '\n': 8}
+        self.dir     = {'L': 9, 'R': 10, 'U': 11, 'D': 12}
+        
+        self.char_to_id = {**self.special, **self.grid, **self.dir}
         self.id_to_char = {v: k for k, v in self.char_to_id.items()}
+        
+        self.pad_id   = self.char_to_id['<PAD>']
+        self.sep_id   = self.char_to_id['<SEP>']
+        self.start_id = self.char_to_id['<START>']
+        self.end_id   = self.char_to_id['<END>']
         self.vocab_size = len(self.char_to_id)
-        self.pad_id     = self.char_to_id['<PAD>']
-        self.sep_id     = self.char_to_id['&']
 
     def encode(self, text: str) -> list:
-        tokens = []; i = 0
-        while i < len(text):
-            if text[i:i+5] == '<PAD>':
-                tokens.append(self.char_to_id['<PAD>'])
-                i += 5
-            elif text[i] in self.char_to_id:
-                tokens.append(self.char_to_id[text[i]])
-                i += 1
-            else:
-                i += 1 
-        return tokens
-
-    def decode(self, ids: list) -> str:
-        return "".join(self.id_to_char.get(i, '?') for i in ids)
+        return [self.char_to_id.get(c, self.pad_id) for c in text if c in self.char_to_id]
 
 class MazeDataset(Dataset):
-    def __init__(self, txt: str, tokenizer: MazeTokenizer, max_length: int):
-        input_ids  = []
-        target_ids = []
+    def __init__(self, txt: str, tokenizer: MazeTokenizer, max_len: int, mode="dencoder"):
+        self.tokenizer = tokenizer
+        self.mode = mode
+        self.samples = []
 
-        mazes = [m for m in txt.split("\n\n")]
+        for block in txt.split("\n\n"):
+            if '<SEP>' not in block: continue
+            m_part, r_part = block.split('<SEP>')
+            
+            m_ids = tokenizer.encode(m_part.strip())
+            r_ids = tokenizer.encode(r_part.strip())
+            
+            if self.mode == "single":
+                # [MAPA, <SEP>, <START>, ROTA, <END>]
+                full = m_ids + [tokenizer.sep_id, tokenizer.start_id] + r_ids + [tokenizer.end_id]
+                full = self._pad(full, max_len + 1)
+                self.samples.append((torch.tensor(full[:-1]), torch.tensor(full[1:])))
+            
+            elif self.mode == "dencoder"
+                # Encoder(MAPA) -> Decoder(START, ROTA) -> Target(ROTA, END)
+                m_in = self._pad(m_ids, max_len)
+                r_in = self._pad([tokenizer.start_id] + r_ids, max_len)
+                r_out = self._pad(r_ids + [tokenizer.end_id], max_len)
+                self.samples.append((torch.tensor(m_in), torch.tensor(r_in), torch.tensor(r_out)))
 
-        for maze in mazes:
-            ids = tokenizer.encode(maze)
-            if len(ids) < 2:
-                continue
-
-            if len(ids) < max_length + 1:
-                ids = ids + [tokenizer.pad_id] * (max_length + 1 - len(ids))
-            else:
-                ids = ids[:max_length + 1]
-
-            input_ids.append(ids[:max_length])
-            target_ids.append(ids[1:max_length+1])
-
-        self.input_ids  = torch.tensor(input_ids,  dtype=torch.long)
-        self.target_ids = torch.tensor(target_ids, dtype=torch.long)
+    def _pad(self, ids, length):
+        return (ids + [self.tokenizer.pad_id] * length)[:length]
 
     def __len__(self):
-        return len(self.input_ids)
+        return len(self.samples)
 
     def __getitem__(self, idx):
-        return self.input_ids[idx], self.target_ids[idx]
+        return self.samples[idx]
 
 def build_dataloader(dsrc: str, max_length: int, batch_size: int = 4,
-                     shuffle: bool = True, drop_last: bool = True,
-                     num_workers: int = 0) -> DataLoader:
+                     shuffle: bool = True, mode: str = "dencoder") -> DataLoader:
 
     with open(dsrc) as f:
         txt = f.read()
     tokenizer = MazeTokenizer()
-    dataset   = MazeDataset(txt, tokenizer, max_length)
+    dataset   = MazeDataset(txt, tokenizer, max_length, mode=mode)
 
     return DataLoader(
         dataset,
         batch_size=batch_size,
         shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers,
         pin_memory=torch.cuda.is_available(),
     )
 
